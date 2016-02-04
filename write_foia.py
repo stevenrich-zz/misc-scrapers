0b23cd3bb8b19e1c022cbc04b2cfcd4d719a92f7
Steven Rich <steven.rich@washpost.com>
2016-02-03 23:56:58 -0500

    Update write_foia.py

--- write_foia.py
+++ write_foia.py
@@ -11,6 +11,28 @@ from docx.enum.style import WD_STYLE
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 import datetime
 
+document.add_paragraph(file_date)
+document.add_paragraph("")
+document.add_paragraph(dept)
+document.add_paragraph(address1)
+document.add_paragraph(address2)
+if address3 != "":
+    document.add_paragraph(address3)
+if address4 != "":
+    document.add_paragraph(address4)
+if address5 != "":
+    document.add_paragraph(address5)
+document.add_paragraph("")
+document.add_paragraph("To whom it may concern:")
+document.add_paragraph("")
+if statute2 == "":
+	  if statute3 != "":
+		    document.add_paragraph("This is a request under " + str(statute1) + u'\xa7' + u'\xa7' + str(statute3) +" I hereby request disclosure of the following information related to each officer who fired shots in the " + str(shooting_date) + " fatal shooting of " + str(deceased) + ":")
+if statute3 == "":
+		document.add_paragraph("This is a request under " + str(statute1) + " I hereby request disclosure of the following information related to each officer who fired shots in the " + str(shooting_date) + " fatal shooting of " + str(deceased) + ":")
+if statute2 != "":
+    document.add_paragraph("This is a request under " + str(statute1) + u'\xa7' + str(statute2) +" I hereby request disclosure of the following information related to each officer who fired shots in the " + str(shooting_date) + " fatal shooting of " + str(deceased) + ":")
+
 document.add_paragraph("If you regard any of the requested records as subject to exemption from required disclosure under the law, I hereby request that you exercise your discretion and disclose them nonetheless.")
 document.add_paragraph("")
 document.add_paragraph("If the reason you are unable to disclose information is that there is an ongoing investigation, please notify me and wait to fulfill the request until such time as the investigation closes.")
