47592a734d75ed48c601f67a0cc4c7d5b0c9fae8
Steven Rich <steven.rich@washpost.com>
2016-02-07 16:09:41 -0500

    Update deep_web.py

--- deep_web.py
+++ deep_web.py
@@ -4,4 +4,6 @@ import requests
 
 socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
 socket.socket = socks.socksocket
+
+#check ip address
 print requests.get("http://icanhazip.com").text
