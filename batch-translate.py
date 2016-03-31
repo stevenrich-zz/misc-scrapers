588c90c0e0e33a33f781884bd37af05cb4769cfd
Steven Rich <steven.rich@washpost.com>
2016-03-31 19:24:33 -0400

    Update batch-translate.py

--- batch-translate.py
+++ batch-translate.py
@@ -1,1 +1,17 @@
 # a script to translate multiple documents using Google translate
+
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(800,600))
+display.start()
+
+url = "translate.google.com"
+
+driver = webdriver.Firefox()
+driver.get(url)
+
+#upload document
+
+driver.quit()
+display.stop()
